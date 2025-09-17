0256929e8bbea3609d46d6a7bac93ffc44e294ed
AbdurRahman-05 <massabdur123@gmail.com>
2025-09-17 21:44:13 +0530

    membership backend

--- db.js
+++ db.js
@@ -11,8 +11,6 @@ dotenv.config({ path: path.resolve(__dirname, '.env') });
 async function connectDB() {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
