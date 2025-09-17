bd14003da8c910e3f4007081daada78a1c2fd95e
AbdurRahman-05 <massabdur123@gmail.com>
2025-09-17 14:24:56 +0530

    modified  backend

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import sendAdminEmailRouter from './sendAdminEmail.js';
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://wetreckindia.netlify.app'],
   credentials: true,
 }));
 app.use(bodyParser.json());
